2a37f9427c1ef80cdb8bd692fc3188ec6ab9dabe
WasefUllah <wasefudvash@gmail.com>
2025-11-05 20:00:33 +0600

    added about section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../Components/Hero";
+import About from "../Components/About";
 
 
 
@@ -27,6 +28,7 @@ const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <About></About>
         </div>
     );
 };
