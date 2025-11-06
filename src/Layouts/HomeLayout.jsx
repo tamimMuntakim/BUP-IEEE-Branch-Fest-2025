af7fda4c68b35b3885c4dd334b53e74039fece59
tamimMuntakimm <130441317+tamimMuntakim@users.noreply.github.com>
2025-11-06 13:35:14 +0600

    navbar modified

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -6,7 +6,7 @@ import Footer from "../Components/Footer";
 const HomeLayout = () => {
     return (
         <>
-            <header className="bg-[#edfdfd] shadow-sm md:shadow-md shadow-secondary sticky top-0 z-50">
+            <header className="bg-gradient-to-b from-black via-gray-900 to-black shadow-sm md:shadow-md shadow-secondary sticky top-0 z-50">
                 <nav className="w-11/12 md:container mx-auto">
                     <NavBar></NavBar>
                 </nav>
