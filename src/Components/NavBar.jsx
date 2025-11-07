ec4239180e8dd43c5cdda752ae84e99ca04dfd63
tamimMuntakimm <130441317+tamimMuntakim@users.noreply.github.com>
2025-11-07 15:36:51 +0600

    changes in navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,32 +2,35 @@ import React from "react";
 import { MdOutlineMenuOpen } from "react-icons/md";
 import { Link, NavLink } from "react-router"; // IMPORTANT: Ensure Link is used
 import "./NavBar.css";
-import { MdAddCircle } from "react-icons/md";
-import { FaMoneyBill } from "react-icons/fa";
+import { FaMoneyBill, FaFacebookSquare } from "react-icons/fa";
 
 const NavBar = () => {
     return (
         <div>
-            <div className="py-1.5 flex justify-between">
-                <div className="">
+            <div className="navbar gap-1 p-0">
+                <div className="navbar-start">
                     <div className="dropdown">
                         <div
                             tabIndex={0}
                             role="button"
-                            className="btn btn-ghost btn-circle md:text-base lg:text-lg xl:text-2xl"
+                            className="btn btn-ghost btn-circle md:text-base lg:text-lg xl:text-2xl p-1"
                         >
                             <MdOutlineMenuOpen className="font-extrabold text-teal-400"></MdOutlineMenuOpen>
                         </div>
                         <ul
                             tabIndex="-1"
-                            className="menu menu-md lg:menu-xl dropdown-content bg-[#edfdfd] rounded-box z-1 mt-3 w-36 md:w-56 p-2 shadow "
+                            className="menu menu-md lg:menu-lg dropdown-content bg-[#edfdfd] rounded-box z-1 mt-3 w-36 md:w-56 p-2 shadow "
                         >
                             <li>
                                 <NavLink to="/" className="nav-links">
                                     Home
                                 </NavLink>
                             </li>
-                            {/* NOTE: CHANGED <a> to <Link> for correct routing */}
+                            <li>
+                                <NavLink to="/blogs" className="nav-links">
+                                    Blogs
+                                </NavLink>
+                            </li>
                             <li>
                                 <Link to="/#about" className="">
                                     About
@@ -59,11 +62,6 @@ const NavBar = () => {
                                 </Link>
                             </li>
                             <li>
-                                <NavLink to="/blogs" className="nav-links">
-                                    Blogs
-                                </NavLink>
-                            </li>
-                            <li>
                                 <Link to="/#contact" className="">
                                     Contact
                                 </Link>
@@ -71,22 +69,36 @@ const NavBar = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="">
+                <div className="navbar-center">
                     <Link
                         to="/"
-                        className="btn btn-ghost text-lg lg:text-xl xl:text-2xl bg-gradient-to-r from-teal-400 to-sky-600 text-transparent bg-clip-text hover:bg-[#edfdfd] hover:shadow-sm hover:border-accent"
+                        className="btn btn-ghost sm:text-lg lg:text-xl xl:text-2xl bg-gradient-to-r from-teal-400 to-sky-600 text-transparent bg-clip-text hover:bg-[#edfdfd] hover:shadow-sm hover:border-accent px-0"
                     >
                         IEEE BranchFest 2025
                     </Link>
                 </div>
-                <div className="">
-                    <div className="hidden sm:gap-2 md:gap-3 sm:flex">
-                        <button className="btn btn-secondary btn-sm md:btn-md text-white hover:text-white">
-                            <MdAddCircle /> Register
-                        </button>
-                        <button className="btn btn-accent btn-sm md:btn-md text-white hover:text-white">
+                <div className="navbar-end">
+                    <div className="sm:gap-2 md:gap-3 flex">
+                        <Link
+                            to="https://www.facebook.com/share/1779mJzeGn/"
+                            target="_blank"
+                            className="btn btn-sm md:btn-md bg-blue-600 text-white border-[#005fd8] hover:bg-blue-700 hidden sm:flex"
+                        >
+                            <FaFacebookSquare /> Event
+                        </Link>
+                        <Link
+                            to="https://www.facebook.com/share/1779mJzeGn/"
+                            target="_blank"
+                            className="btn btn-xs bg-blue-600 text-white border-[#005fd8] hover:bg-blue-700 sm:hidden"
+                        >
+                            <FaFacebookSquare />
+                        </Link>
+                        <Link
+                            to="/#partner-with-us"
+                            className="btn btn-accent btn-sm md:btn-md text-white hover:text-white hidden sm:flex"
+                        >
                             <FaMoneyBill /> Sponsor
-                        </button>
+                        </Link>
                     </div>
                 </div>
             </div>
